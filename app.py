a4a84949c70761521bd5d22c3d17ab04333d5f9b
ARUP GOPE <gopea999@gmail.com>
2025-08-24 21:31:54 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from src.langgraph.main import load_langgraph_agentical_app
+from src.langgraphagenticai.main import load_langgraph_agenticai_app
 
 if __name__=="__main__":
-    load_langgraph_agentical_app()
+    load_langgraph_agenticai_app()
